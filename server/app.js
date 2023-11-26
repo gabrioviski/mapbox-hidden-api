902dedac3f0f6b2b7516906d79f78e03dbdc9909
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-11-26 19:12:07 -0300

    passado um parametro query para reverse geocoding

--- server/app.js
+++ server/app.js
@@ -11,8 +11,14 @@ app.use(cors({
 
 app.get('/api/:query', async (req, res) => {
     const accessToken = process.env.MAPBOX_KEY
-    const query = req.params.query
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${accessToken}&limit=3&language=pt`
+    const { query } = req.params
+    const { reverse } = req.query
+    let limit = 3
+    console.log(query, reverse)
+    if (reverse) {
+        limit = 1
+    }
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${accessToken}&limit=${limit}&language=pt`
     const resObj = await fetch(url)
     const dataRes = await resObj.json()
     res.json(dataRes)
