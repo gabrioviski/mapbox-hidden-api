9b2814cabd55579f3781245988f8ebacaf3c2ec9
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-11-26 21:00:18 -0300

    adicionado bbox

--- server/app.js
+++ server/app.js
@@ -14,11 +14,12 @@ app.get('/api/:query', async (req, res) => {
     const { query } = req.params
     const { reverse } = req.query
     let limit = 3
+    const bbox = "-53.305664,-25.204941,-45.439453,-19.849394"
     console.log(query, reverse)
     if (reverse) {
         limit = 1
     }
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${accessToken}&limit=${limit}&language=pt`
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${accessToken}&limit=${limit}&language=pt&bbox=${bbox}`
     const resObj = await fetch(url)
     const dataRes = await resObj.json()
     res.json(dataRes)
