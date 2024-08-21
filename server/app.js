43bd36db83baae19ecb410afc0d1a00402b0d537
gabrioviski <117327064+gabrioviski@users.noreply.github.com>
2024-08-21 08:04:28 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,12 +5,19 @@ const app = express()
 require('dotenv').config()
 const accessToken = process.env.MAPBOX_KEY
 
+const whitelist = ['https://fretus.onrender.com', 'https://fretus-ohio.onrender.com']
 const corsOptions = {
     // colocar a URL sem a "/".
     // Exemplo Certo: https://site.com
     // Exemplo Errado: https://site.com/
     
-    origin: 'https://fretus.onrender.com',
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions))
